e56c821d90bba59dae4258897244abc4b7d588b4
MarianBaba <marianbaba16@gmail.com>
2022-01-10 17:38:58 +0100

    rubrica application methods

--- src/it/beije/pascal/rubrica/RubricaApplication.java
+++ src/it/beije/pascal/rubrica/RubricaApplication.java
@@ -1,51 +1,89 @@
 package it.beije.pascal.rubrica;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+//import java.util.ArrayList;
+//import java.util.List;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class RubricaApplication {
 	
-	private List<Contatto> contatti = new ArrayList<>();
+	static Scanner scan = new Scanner(System.in);
 	
-	public void listContacts() { //vedi lista contatti, con possibilità di ordinare per nome e cognome a scelta
-		for(Contatto utente : contatti) {
-			System.out.println("-------------------");
-			System.out.println("Nome: " + utente.getNome());
-			System.out.println("Cognome: " + utente.getCognome());
-			System.out.println("Telefono: " + utente.getTelefono());
-			System.out.println("Email: " + utente.getEmail());
-			System.out.println("Note: " + utente.getNote());
+	//private List<Contatto> contatti = new ArrayList<>();
+	
+	private static Connection getCon() throws Exception {
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/rubrica?serverTimezone=CET", "root", "baba");
+		return con;
+	}
+	
+	public static void listContacts() throws Exception { //vedi lista contatti, con possibilità di ordinare per nome e cognome a scelta
+		Connection con = getCon();
+		
+		Statement st = con.createStatement();
+		ResultSet rs = st.executeQuery("SELECT * FROM contatti");
+		
+		while(rs.next()) {
+			System.out.println("id: " + rs.getInt("id"));
+			System.out.print("cognome: " + rs.getString("cognome"));
+			System.out.println(", nome: " + rs.getString("nome"));
+			System.out.print("telefono: " + rs.getString("telefono"));
+			System.out.println(", email: " + rs.getString("email"));
+			System.out.println("note: " + rs.getString("note") + "\n");
 		}
 	}
 	 
-	public void lookForContact() { //cercare un contatto
+	private static void lookForContact() throws Exception { //cercare un contatto
+		Connection con = getCon();
 		
+		Statement st = con.createStatement();
+		ResultSet rs = st.executeQuery("SELECT * FROM contatti");
 	}
 	
-	public void newContact() { //cercare un contatto
+	private static void newContact() throws Exception { //cercare un contatto
+		Connection con = getCon();
 		
+		Statement st = con.createStatement();
+		ResultSet rs = st.executeQuery("SELECT * FROM contatti");
 	}
 	
-	public void updateContact() { //modificare un contatto
+	private static void updateContact() throws Exception { //modificare un contatto
+		Connection con = getCon();
 		
+		Statement st = con.createStatement();
+		ResultSet rs = st.executeQuery("SELECT * FROM contatti");
 	}
 	
-	public void deleteContact() { //cancellare un contatto
+	private static void deleteContact() throws Exception { //cancellare un contatto
+		Connection con = getCon();
 		
+		Statement st = con.createStatement();
+		ResultSet rs = st.executeQuery("SELECT * FROM contatti");
 	}
 	
-	public void findAllDuplicate() { //trovare i contatti duplicati
+	private static void findAllDuplicate() throws Exception { //trovare i contatti duplicati
+		Connection con = getCon();
 		
+		Statement st = con.createStatement();
+		ResultSet rs = st.executeQuery("SELECT * FROM contatti");
 	}
 	
-	public void mergeDuplicate() { //unire i contatti duplicati
+	private static void mergeDuplicate() throws Exception { //unire i contatti duplicati
+		Connection con = getCon();
 		
+		Statement st = con.createStatement();
+		ResultSet rs = st.executeQuery("SELECT * FROM contatti");
 	}
 	
-	public static void showMenu() {
+	private static void showMenu() throws Exception {
 		LocalDateTime ldt = LocalDateTime.now();
-		System.out.println("---RUBRICA---\n" + ldt + "\nScegli una delle seguenti operazioni:\n"
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd - MM - yyyy   HH:mm ");
+		System.out.println("---RUBRICA---\n" + ldt.format(dtf) + "\nScegli una delle seguenti operazioni:\n"
 				+ "1. visualizza tutti i contatti\n"
 				+ "2. cerca un contatto\n" 
 				+ "3. crea un contatto\n"
@@ -53,10 +91,31 @@ public class RubricaApplication {
 				+ "5. cancella un contatto\n"
 				+ "6. trova contatti duplicati\n"
 				+ "7. unisci contatti duplicati");
-	}	
 	
-	public static void main(String[] args) {
-		showMenu();
+		int choice = scan.nextInt();
+		
+		switch(choice) {
+			case 1: listContacts();
+				break;
+			case 2: lookForContact();
+				break;
+			case 3: newContact();
+				break;
+			case 4: updateContact();
+				break;
+			case 5: deleteContact();
+				break;
+			case 6: findAllDuplicate();
+				break;
+			case 7: mergeDuplicate();
+				break;
+			default: System.out.println("Scegli un'opzione valida");
+				showMenu();
+				break;
+		}
 	}
 	
+	public static void main(String[] args) throws Exception {
+		showMenu();
+	}
 }
