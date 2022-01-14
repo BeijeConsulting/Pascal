ef0d4087b89e5f360583fc43cae0b8eae1ec9578
MarianBaba <marianbaba16@gmail.com>
2022-01-14 18:26:34 +0100

    commento

--- src/it/beije/pascal/XMLParser2/DocumentNode.java
+++ src/it/beije/pascal/XMLParser2/DocumentNode.java
@@ -1,12 +1,10 @@
 package it.beije.pascal.XMLParser2;
 
+//questa classe non viene usata nel parser principale
 public class DocumentNode {
 	//cosa deve avere un nodo?
 	//1. dobbiamo sapere se è un elemento oppure no
 	//2. eventualmente può avere del contenuto testuale
-	
-	//tutti gli elementi sono nodi, ma non tutti i nodi sono elementi, di conseguenza ha senso che Element sia figlio di Node
-	//quindi dobbiamo sapere anche se un nodo è un elemento oppure un "nodo sporco"
 	
 	private String contenuto;
 	private boolean isElemento;
