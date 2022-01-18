877db651dbed7b153fc1149cfc784a3c679f7f58
Emanuele <emacorona77@gmail.com>
2022-01-18 17:53:04 +0100

    operazioni CRUD jpa

--- src/it/beije/pascal/jpa/crud/CRUD.java
+++ src/it/beije/pascal/jpa/crud/CRUD.java
@@ -12,7 +12,9 @@ import it.beije.pascal.rubrica.Contatto;
 public class CRUD {
 	public static void main(String[] args) {
 		//read();
-		delete(11150);
+		//delete(11150);
+		//update(11166);
+		save();
 
 	}
 
@@ -47,6 +49,39 @@ public class CRUD {
 		transaction.commit();
 		entityManger.close();
 		
+	}
+	
+	private static void update(int id) {
+		EntityManager entityManager = EntityManagerProvider.getEntityManager();
+		EntityTransaction transaction = entityManager.getTransaction();
+		transaction.begin();
+		
+		// JPQL
+		String jpql = "SELECT c FROM Contatto AS c WHERE id = :id";
+		Query query = entityManager.createQuery(jpql);
+		query.setParameter("id", id);
+		Contatto contatto = (Contatto) query.getSingleResult();
+		contatto.setCognome("modificato");
+		contatto.setNome("modificato");
+		contatto.setTelefono("modificato");
+		contatto.setEmail("modificato");
+		contatto.setNote("modificato");
+		// Stessa funzionalità del save di session
+		entityManager.persist(contatto);
+		transaction.commit();
+		entityManager.close();
+	}
+	
+	private static void save() {
+		Contatto contatto = new Contatto("nuovo", "nuovo", "nuovo", "nuovo", "nuovo");
+		EntityManager entityManger = EntityManagerProvider.getEntityManager();
+		EntityTransaction transaction = entityManger.getTransaction();
+		transaction.begin();
+		
+		entityManger.persist(contatto);
+		
+		transaction.commit();
+		entityManger.close();
 	}
 
 }
