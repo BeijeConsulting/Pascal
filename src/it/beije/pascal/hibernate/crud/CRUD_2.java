267fd90f0a2dab0e75e6a94c510095bc1624e59c
Emanuele <emacorona77@gmail.com>
2022-01-18 17:24:11 +0100

    crud versione 2

---
+++ src/it/beije/pascal/hibernate/crud/CRUD_2.java
@@ -0,0 +1,78 @@
+package it.beije.pascal.hibernate.crud;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
+import it.beije.pascal.hibernate.util.SessionProvider;
+import it.beije.pascal.rubrica.Contatto;
+
+public class CRUD_2 {
+
+	public static void main(String[] args) {
+		update();
+
+	}
+	
+
+	private static void read() {
+		// Creazione oggetti per lavorare con il database
+		Configuration configuration = new Configuration().configure().addAnnotatedClass(Contatto.class);
+		SessionFactory sessionFactory = configuration.buildSessionFactory();
+		Session session = sessionFactory.openSession();
+		
+		String sql = "SELECT c FROM Contatto AS c";
+
+		Query<Contatto> query = session.createQuery(sql);
+		List<Contatto> contatti = query.getResultList();
+
+		for (Contatto contatto : contatti) {
+			System.out.println(contatto);
+		}
+	}
+	
+	private static void save() {
+		Contatto contatto = new Contatto("nuovo", "nuovo", "nuovo", "nuovo", "nuovo");
+		Session session = SessionProvider.getInstance().getSession();
+		// Transaction transaction = session.getTransaction();
+		// transaction.begin();
+
+		// Fa entrambi
+		Transaction transaction = session.beginTransaction();
+		System.out.println("Contatto pre: " + contatto);
+		session.save(contatto);
+		System.out.println("Contatto post: " + contatto);
+		transaction.commit();		
+	}
+	
+	
+	private static void update() {
+		Session session = SessionProvider.getInstance().getSession();
+		Transaction transaction = session.beginTransaction();		
+		String sql = "UPDATE Contatto SET nome = 'modificato' WHERE id = '11151' ";
+		Query<Contatto> query = session.createQuery(sql);
+		query.executeUpdate();
+		transaction.commit();
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
