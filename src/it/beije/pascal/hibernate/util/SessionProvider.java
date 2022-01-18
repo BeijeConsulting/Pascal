aa653c5430b3f84d489bd46dbce3a6e324d00c4c
Emanuele <emacorona77@gmail.com>
2022-01-18 17:23:43 +0100

    Session provider

---
+++ src/it/beije/pascal/hibernate/util/SessionProvider.java
@@ -0,0 +1,27 @@
+package it.beije.pascal.hibernate.util;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import it.beije.pascal.jdbc.util.DataSource;
+import it.beije.pascal.rubrica.Contatto;
+
+public class SessionProvider {
+	private Configuration configuration = new Configuration().configure().addAnnotatedClass(Contatto.class);
+	private SessionFactory sessionFactory = configuration.buildSessionFactory();
+	private Session session = sessionFactory.openSession();
+
+	private static SessionProvider instance;
+
+	public static SessionProvider getInstance() {
+		if (instance == null) {
+			instance = new SessionProvider();
+		}
+		return instance;
+	}
+
+	public Session getSession() {
+		return session;
+	}
+}
