f58bb36d44c5b5c37e8ee0503fd48248632b211f
ema29 <ema29@DESKTOP-5H4N041>
2022-01-19 09:40:55 +0100

    import/esport da csv e xml

--- src/it/beije/pascal/jdbc/rubrica/GestoreRubrica.java
+++ src/it/beije/pascal/jdbc/rubrica/GestoreRubrica.java
@@ -28,10 +28,10 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
+import it.beije.pascal.Contatto;
 import it.beije.pascal.file.util.FileUtil;
 import it.beije.pascal.jdbc.util.DBUtil;
 import it.beije.pascal.jdbc.util.DataSource;
-import it.beije.pascal.rubrica.Contatto;
 
 public class GestoreRubrica {
 
