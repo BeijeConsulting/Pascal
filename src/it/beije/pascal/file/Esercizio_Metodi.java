1b2cd9677ed8d5dbf60f62c66bd1b7cbfc6e9412
ema29 <ema29@DESKTOP-5H4N041>
2022-01-19 12:53:54 +0100

    implementazione metodi csv e xml

--- src/it/beije/pascal/file/Esercizio_Metodi.java
+++ src/it/beije/pascal/file/Esercizio_Metodi.java
@@ -23,7 +23,7 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
-import it.beije.pascal.Contatto;
+import it.beije.pascal.bean.Contatto;
 import it.beije.pascal.file.util.FileUtil;
 
 public class Esercizio_Metodi {
