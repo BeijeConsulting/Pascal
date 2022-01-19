76c1d566f528bf8dae2c67af823a776a6b658e12
ema29 <ema29@DESKTOP-5H4N041>
2022-01-19 12:53:34 +0100

    lettura/scrittura su xml

--- src/it/beije/pascal/file/xml/WriteReadXML.java
+++ src/it/beije/pascal/file/xml/WriteReadXML.java
@@ -19,7 +19,7 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
-import it.beije.pascal.Contatto;
+import it.beije.pascal.bean.Contatto;
 
 public class WriteReadXML {
 
