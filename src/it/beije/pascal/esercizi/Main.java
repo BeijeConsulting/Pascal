22b1006119b849d2ad5ffffc7b47851f4a25b946
Andrea Gliori <andrea.gliori@gmail.com>
2022-01-21 15:45:45 +0100

    new commit

--- src/it/beije/pascal/esercizi/Main.java
+++ src/it/beije/pascal/esercizi/Main.java
@@ -1,6 +1,7 @@
 package it.beije.pascal.esercizi;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.xml.parsers.ParserConfigurationException;
@@ -13,17 +14,19 @@ import it.beije.pascal.rubrica.Contatto;
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-//		IOBeanUtil io = new IOBeanUtil();
-//		List<Contatto> contatti = io.loadRubricaFromXML("rubrica.xml");
-//		io.writeRubricaXML(contatti, "rubrica.xml");
+		//		IOBeanUtil io = new IOBeanUtil();
+		//		List<Contatto> contatti = io.loadRubricaFromXML("rubrica.xml");
+		//		io.writeRubricaXML(contatti, "rubrica.xml");
+
+		//		ConnectionPull.getInstance();
+		//		System.out.println(ConnectionPull.getConnection());
+		//		System.out.println(ConnectionPull.getConnection());
+		//		System.out.println(ConnectionPull.getConnection());
+		//		System.out.println(ConnectionPull.getConnection());
+		//		System.out.println(ConnectionPull.getConnection());
+		//		System.out.println(ConnectionPull.getConnection());
+
 		
-		ConnectionPull.getInstance();
-		System.out.println(ConnectionPull.getConnection());
-		System.out.println(ConnectionPull.getConnection());
-		System.out.println(ConnectionPull.getConnection());
-		System.out.println(ConnectionPull.getConnection());
-		System.out.println(ConnectionPull.getConnection());
-		System.out.println(ConnectionPull.getConnection());
 	}
 
 }
